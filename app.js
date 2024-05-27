123ff0712ed39811e7d9c2ed27b8045f4517bb3c
Macdonald Nkhata <nkhatamac27@gmail.com>
2024-05-27 23:17:02 +0200

    implemented google sign in endpoint

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
-// import session from 'express-session';
-// import passport from './config/passport.js';
+import session from 'express-session';
+import passport from './config/passport.js';
 import auth from './routes/auth.js';
 import mongoose from 'mongoose';
 
@@ -10,50 +10,18 @@ const app = express();
 
 app.use(express.json());
 
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: true,
-//   })
-// );
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// // Login
-// app.get('/login', (req, res) => {
-//   res.send('<a href="/auth/google">Login with Google</a>');
-// });
-
-// app.get(
-//   '/auth/google',
-//   passport.authenticate('google', { scope: ['profile'] })
-// );
-
-// app.get(
-//   'auth/google/callback',
-//   passport.authenticate('google', { failureRedirect: '/login' }),
-//   (req, res) => {
-//     res.redirect('/');
-//   }
-// );
-
-// app.get('/', (req, res) => {
-//   if (req.isAuthenticated()) {
-//     res.send(`Hello, ${req.user.displayName}`);
-//   } else {
-//     res.send('Hello, guest! Please <a href="/login">login</a>');
-//   }
-// });
-
-// app.get('/logout', (req, res) => {
-//   req.logout();
-//   res.redirect('/');
-// });
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Connect to DB
-
 app.use('/api/v1/auth/', auth);
 
 const uri = process.env.DB_CONNECTION;
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,25 @@
 import express from 'express';
+import passport from '../config/passport.js';
 import { register, login } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
+
+router.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  (req, res) => {
+    res.redirect('/');
+  }
+);
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
 
 export default router;
