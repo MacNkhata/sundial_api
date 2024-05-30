3ce67e749cdb4abc7026362a38c65919e342144c
Macdonald Nkhata <nkhatamac27@gmail.com>
2024-05-31 00:06:11 +0200

    refactored google auth to use database

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import User from '../models/User.js';
 
 passport.use(
   new GoogleStrategy(
@@ -8,17 +9,21 @@ passport.use(
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
-    (accessToken, refreshToken, profile, done) => {
-      let user = users.find((user) => user.googleId === profile.id);
-      if (!user) {
-        user = {
-          id: users.length + 1,
-          googleId: profile.id,
-          displayName: profile.displayName,
-        };
-        users.push(user);
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+
+        if (!user) {
+          user = new User({
+            googleId: profile.id,
+            displayName: profile.displayName,
+          });
+          await user.save();
+        }
+        return done(null, user);
+      } catch (error) {
+        return done(null, error);
       }
-      return done(null, user);
     }
   )
 );
@@ -28,7 +33,7 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-  const user = users.find((user) => user.id === id);
+  const user = user.findById((user) => user.id === id);
   done(null, user);
 });
 
